fc8d0158d077573720edaae7b9af36558787f262
emon <tj.emon33@gmail.com>
2025-04-10 11:06:16 +0600

    url-state

--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 ## SPA
 
 ![alt text](single-page-app.png)
+
+## URL for state management
+
+![alt text](url-state-management.png)
+
+### Params & Query string
+
+![alt text](params-query-string.png)
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -6,7 +8,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayOut from "./pages/AppLayOut";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountriesList from "./components/CountriesList";
 
 const BASE_URL = "http://localhost:9002/cities";
 
@@ -44,7 +46,10 @@ function App() {
             path="cities"
             element={<CityList isLoading={isLoading} cities={cities} />}
           />
-          <Route path="countries" element={<p>countries</p>} />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
