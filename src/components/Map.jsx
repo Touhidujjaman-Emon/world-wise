d9b1ae06be5ae3dc0838df55cf4943e7239fee38
emon <tj.emon33@gmail.com>
2025-04-11 09:01:59 +0600

    setparams

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,16 @@
 import styles from "./Map.module.css";
+import { useSearchParams } from "react-router-dom";
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat") || 0;
+  const lng = searchParams.get("lng") || 0;
+  return (
+    <div className={styles.mapContainer}>
+      <h1>
+        lat: {lat}, lng:{lng}
+      </h1>
+    </div>
+  );
 }
 
 export default Map;
