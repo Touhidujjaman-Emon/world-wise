2cb10d1fcf29ed3ac60554a27181b184e0d802f8
emon <tj.emon33@gmail.com>
2025-05-11 21:30:14 +0600

    auth s 1

---
+++ src/Contexts/FakeAuthContext.jsx
@@ -0,0 +1,53 @@
+import { use } from "react";
+import { createContext, useContext } from "react";
+import { useReducer } from "react";
+
+const AuthContext = createContext();
+
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+      };
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
+
+function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  function login(email, password) {}
+
+  function logout() {}
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+}
