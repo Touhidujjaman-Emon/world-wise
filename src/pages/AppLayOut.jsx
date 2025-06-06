37951f82ab8e2b770ab48526c5409b8261babd1b
emon <tj.emon33@gmail.com>
2025-06-06 13:45:14 +0600

    fixed build

--- src/pages/AppLayOut.jsx
+++ src/pages/AppLayOut.jsx
@@ -1,6 +1,6 @@
 import Sidebar from "../components/Sidebar";
 import Map from "../components/Map";
-import styles from "./AppLayOut.module.css";
+import styles from "./AppLayout.module.css";
 import User from "../components/User";
 
 function AppLayOut() {
